7ccc8b2c01af428a89815437813d08de69c03bf8
Ngwube Miracle Onyedika <92275870+Mongwube@users.noreply.github.com>
2024-09-21 14:05:37 +0100

    Update login.jsx

--- login.jsx
+++ login.jsx
@@ -1,54 +1,60 @@
-var db // indicate index DB
+var db // It symbolises IndexDb
 
-// initialize indexDB for login page
+// Initialize indexDB for Login Page
+
 window.onload = function(){
-    //open indexDB
     let request = indexedDB.open('UserDB', 1);
 
     request.onerror = function(){
-        console.log('Database Failed to Open.')
+        console.log('Database failed to open')
     }
 
     request.onsuccess = function(){
-        console.log('Database Opend Successfully.')
-        db = request.result;
+        console.log('Database opened succesfully')
+        db = request.result
     }
 }
 
 document.querySelector('#login-form').addEventListener('submit', function(e){
     e.preventDefault()
-    // input from HTML
     const loginEmail = document.querySelector('#login-email')
     const loginPassword = document.querySelector('#login-password')
-    const loginMsg = document.querySelector('#login-msg')
 
-    // set up a query code or program.
+    const loginMsg = document.querySelector('.login-msg')
 
-    let transaction = db.transaction(['users'], 'readonly') // read the value of the database
-    let objectStore = transaction.objectStore('users')//get all the values of users in the database
-    let index = objectStore.index('email')// get email of users in the database
-    let request = index.get(loginEmail.value)//get the value of the email of the logingin user
 
-    request.onsuccess =function(){
-        if(request.result && request.result.loginPassword === loginPassword.value){
+    // Setup a query code
+
+    let transaction = db.transaction(['users'], 'readonly')
+    let objectStore = transaction.objectStore('users')
+    let index = objectStore.index('email')
+    let request = index.get(loginEmail.value)
+    
+
+    request.onsuccess = function(){
+      
+        if(request.result && request.result.password === loginPassword.value){
             window.location.href = 'dashboard.html'
         }else{
-            loginMsg.classList.add('error','show')
+            loginMsg.classList.add('error', 'show')
             loginMsg.innerHTML = 'Invalid email or password'
+    
             setTimeout(()=>{
-            loginMsg.classList.remove('error','show')
-            loginMsg.innerHTML = ''
-            })
+               loginMsg.classList.remove('error', 'show')
+               loginMsg.innerHTML = ''
+            }, 1000)
         }
     }
-    request.onerror = function(){
-        console.log('error during login')
 
-        loginMsg.classList.add('error','show')
-        loginMsg.innerHTML = 'an error occured during login'
-        setTimeout(()=>{
-        loginMsg.classList.remove('error','show')
-        loginMsg.innerHTML = ''
-        })
-    }
+  request.onerror = function(){
+    console.log('error during login')
+    loginMsg.classList.add('error', 'show')
+            loginMsg.innerHTML = 'an error occured during login'
+    
+            setTimeout(()=>{
+               loginMsg.classList.remove('error', 'show')
+               loginMsg.innerHTML = ''
+            }, 1000)
+  }
+   
 })
